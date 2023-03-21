407970240bde816380bdf4ae06cfe2d2f2324d5e
Andrew Leon <andrew.c.leon.18@gmail.com>
2023-03-20 20:45:15 -0500

    Configure node sass and initial variables

--- drew-leon-ui/src/App.test.tsx
+++ drew-leon-ui/src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App.tsx', () => {
+
+  it('Renders the under construction image', () => {
+    render(<App />);
+    const underConstructionImage = screen.getByTestId("website-under-construction-image")
+    expect(underConstructionImage).toBeInTheDocument()
+  });
+})
