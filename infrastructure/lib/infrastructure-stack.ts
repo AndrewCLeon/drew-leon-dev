5dba819ac5e778211dfaee6564a35f4e9c6a3cb4
Andrew Leon <andrew.c.leon.18@gmail.com>
2023-03-21 00:25:21 -0500

    Re-Configure Route53, SSL, Distro to use www sub domain

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins'
 import { PublicHostedZone } from 'aws-cdk-lib/aws-route53'
-import { CnameRecord } from 'aws-cdk-lib/aws-route53'
+import { ARecord } from 'aws-cdk-lib/aws-route53'
 import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager'
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets'
 
 export type WebsiteDistributionProps = cdk.StackProps & {
   websiteDomainName: string
@@ -14,7 +15,6 @@ export class WebsiteDistribution extends cdk.Stack {
     super(scope, id, props)
 
     const domainName = props.websiteDomainName
-
     const bucket = new cdk.aws_s3.Bucket(this, 'TheWebsiteBucket', {
       bucketName: `${cdk.Aws.ACCOUNT_ID}-${domainName}`
     })
@@ -23,6 +23,9 @@ export class WebsiteDistribution extends cdk.Stack {
 
     const sslCertificate = new Certificate(this, 'TheCertificate', {
       domainName,
+      subjectAlternativeNames: [
+        `www.${domainName}`
+      ],
       validation: CertificateValidation.fromDns(zone)
     })
 
@@ -31,14 +34,16 @@ export class WebsiteDistribution extends cdk.Stack {
         origin: new origins.S3Origin(bucket)
       },
       certificate: sslCertificate,
-      domainNames: [zone.zoneName],
+      domainNames: [
+        `www.${domainName}`
+      ],
       defaultRootObject: 'index.html'
     })
 
-    new CnameRecord(this, 'TheCnameRecord', {
+    new ARecord(this, 'TheARecord', {
       zone,
-      domainName: distribution.domainName,
       recordName: `www.${domainName}`,
+      target: cdk.aws_route53.RecordTarget.fromAlias(new CloudFrontTarget(distribution))
     })
   }
 }
